5ed0f3e9d07cf29732de3aa33f34bc418ad4245f
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-06-29 21:06:36 +0200

    refactor sum function to take any size of array

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -3,12 +3,16 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+	t.Run("collection of 5 numbers", func(t *testing.T) {
 
-	if got != want {
-		t.Errorf("got %d want %d give, %v", got, want, numbers)
-	}
+		numbers := []int{1, 2, 3, 4, 5}
+
+		got := Sum(numbers)
+		want := 15
+
+		if got != want {
+			t.Errorf("got %d want %d give, %v", got, want, numbers)
+		}
+	})
 }
