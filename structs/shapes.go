fd838528ce945526e5cacf05ad36f6a251cca991
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-07-01 20:04:59 +0200

    make test output more helpful

--- structs/shapes.go
+++ structs/shapes.go
@@ -11,6 +11,11 @@ type Rectangle struct {
 	Heigth float64
 }
 
+type Triangle struct {
+	Base   float64
+	Heigth float64
+}
+
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Heigth
 }
@@ -21,6 +26,10 @@ type Circle struct {
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Heigth) * 0.5
 }
 
 // func Perimeter(rectangle Rectangle) float64 {
