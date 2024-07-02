0a744aac3028f5a3e683c20e9f37813d4ec7edc7
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-07-02 22:19:54 +0200

    refactor dictionary implementation to use a map instead of a struct

---
+++ maps/dictionary.go
@@ -0,0 +1,23 @@
+package main
+
+import "errors"
+
+type Dictionary map[string]string
+
+var (
+	ErrNotFound   = errors.New("could not find the word you were looking for")
+	ErrWordExists = errors.new("cannot add word because it already exists")
+)
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+func (d Dictionary) Add(input, definition string) {
+	d[input] = definition
+}
