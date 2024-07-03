a3168e40ecd60321784a41c605bfbca175a8ff2d
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-07-03 20:02:18 +0200

    update add function and refactor error messages

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,13 +1,17 @@
 package main
 
-import "errors"
-
 type Dictionary map[string]string
 
 var (
-	ErrNotFound   = errors.New("could not find the word you were looking for")
-	ErrWordExists = errors.new("cannot add word because it already exists")
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
 )
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -18,6 +22,17 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(input, definition string) {
-	d[input] = definition
+func (d Dictionary) Add(input, definition string) error {
+	_, err := d.Search(input)
+
+	switch err {
+	case ErrNotFound:
+		d[input] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
 }
